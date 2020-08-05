adbd0e25663dd8c9b106a68350a23ae29315e835
Ryan Ackerman <ryansackerman@gmail.com>
2020-08-05 14:29:33 -0700

    init commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import { Button, Provider as BumbagProvider } from 'bumbag';
+const MyApp = () => (
+    <BumbagProvider>
+    <Button>
+    Hello world!
+    </Button>
+    </BumbagProvider>
+);
+
+import { Button, Provider as BumbagProvider } from 'bumbag';
+const MyApp = () => (
+    <BumbagProvider>
+        <Button>
+            Hello world!
+        </Button>
+    </BumbagProvider>
+)
