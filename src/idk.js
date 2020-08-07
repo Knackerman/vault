9c9355a97640c6411ea82e2b15cd6025b8184760
Ryan Ackerman <ryansackerman@gmail.com>
2020-08-07 09:03:51 -0700

    added files to match folder structure of a repo provided in a udemy course

---
+++ src/idk.js
@@ -0,0 +1,15 @@
+import { Provider as BumbagProvider } from 'bumbag';
+const App = () => (
+  <BumbagProvider>
+    Hello world!
+  </BumbagProvider>
+);
+
+import { Button, Provider as BumbagProvider } from 'bumbag';
+const MyApp = () => (
+  <BumbagProvider>
+    <Button>
+      Hello world!
+        </Button>
+  </BumbagProvider>
+)
